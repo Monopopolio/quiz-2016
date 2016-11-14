3e9d89d5e17c2f9b3bf2ebf50d43a2dffbb38bac
Monopopolio <carlosellopez@hotmail.com>
2016-11-14 09:39:22 +0100

    Dando pistas al usuario

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,35 +14,32 @@ exports.load = function(req, res, next, quizId){
 //GET /quizes/:id
 exports.show = function (req, res) {
 	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show',{quiz: req.quiz, errors:[]});
+		var pista = "";
+		for (var i = 0; i < req.query.fallos; i++) {
+			pista = pista + req.quiz.respuesta.charAt(i);
+		}
+		res.render('quizes/show',{quiz: req.quiz, errors:[], letra: pista, fallos: req.query.fallos});
 	})
 };
 
 
 //GET /quizes/:id/answer
 exports.answer = function(req, res){
+	var fallos = req.query.fallos;
 	var resultado = 'Incorrecto';
-	if (req.query.respuesta === req.quiz.respuesta){
+	if (req.query.respuesta.toUpperCase() === req.quiz.respuesta.toUpperCase()){
 		resultado = 'Correcto';
+	}else{
+		fallos++;
 	}
 	res.render('quizes/answer',{ quiz: req.quiz,
-		respuesta: resultado,
+		respuesta: resultado ,
+		letra: req.quiz.respuesta.charAt(0),
+		fallos: fallos,
 		errors: []
 	}
 	);
 };
-
-
-/*//GET /quizes/:id/answer
-exports.answer = function(req, res){
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta.toUpperCase() === req.quiz.respuesta.toUpperCase()){
-			res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Correcto'});
-		} else {
-			res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto'});
-		}
-	})
-};*/
 
 //GET /quizes
 exports.index= function(req, res){
@@ -74,22 +71,3 @@ exports.create = function(req, res) {
 	}
 	);
 };
-
-
-/*exports.create = function(req, res) {
-	var quiz = models.Quiz.build( req.body.quiz );
-	quiz.validate().then(function(err){
-		if(err){
-			res.render('quizes/new', {quiz: quiz, errors: err.errors});
-		} else{
-			quiz.save({fields: ["pregunta", "respuesta"]})
-
-		}
-	}
-	);
-};
-// guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) //redireccion HTTP (URL relativo) lista de preguntas
-};*/
