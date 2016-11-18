b958bba260431f67cb25c006ae1e1037b8313508
Monopopolio <carlosellopez@hotmail.com>
2016-11-18 12:13:46 +0100

    con comentarios

--- models/models.js
+++ models/models.js
@@ -9,9 +9,18 @@ var sequelize = new Sequelize(null, null, null, {
 	storage: "quiz.sqlite"
 });
 
-//Importar la definición de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
-exports.Quiz = Quiz; // exportar definición de la tabla Quiz
+
+
+// Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
@@ -32,3 +41,8 @@ sequelize.sync().then(function() {
 		};
 	});
 });
+
+
+
+exports.Quiz = Quiz; // exportar tabla Quiz
+exports.Comment = Comment;
